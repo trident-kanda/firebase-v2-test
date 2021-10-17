9c18b524ccb3271f2dd1f5746ad00ef401e74fd8
trident-kanda <tridentkandayuki@gmail.com>
2021-10-18 00:18:41 +0900

    getData

--- firebase/firestore.ts
+++ firebase/firestore.ts
@@ -1,15 +1,22 @@
-import { collection, addDoc } from "@firebase/firestore";
+import { collection, addDoc, getDocs } from "@firebase/firestore";
 import { db } from "./util";
 
 //データを追加
-const addData = async() => {
-    try{
-        const docRef = await addDoc(collection(db, "users"),{
-            first:"Tanaka",
-            last:"Taro",
-            born: 1999
-        });
-    }catch(e) {
-        console.error("err")
-    }
-}
+const addData = async () => {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Tanaka",
+      last: "Taro",
+      born: 1999,
+    });
+  } catch (e) {
+    console.error("err");
+  }
+};
+
+const getData = async () => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
+};
