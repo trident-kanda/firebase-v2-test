e615a135db165aca93065328844cb53790077989
trident-kanda <tridentkandayuki@gmail.com>
2021-10-19 10:05:17 +0900

    type

--- firebase/firestore.ts
+++ firebase/firestore.ts
@@ -2,20 +2,24 @@ import { collection, addDoc, getDocs } from "@firebase/firestore";
 import { db } from "./util";
 
 //データを追加
-const addData = async () => {
+interface addDataProps {
+  dbName: string;
+  data: object;
+}
+
+interface getDataProps {
+  dbName: string;
+}
+const addData = async ({ dbName, data }: addDataProps) => {
   try {
-    const docRef = await addDoc(collection(db, "users"), {
-      first: "Tanaka",
-      last: "Taro",
-      born: 1999,
-    });
+    const docRef = await addDoc(collection(db, dbName), data);
   } catch (e) {
     console.error("err");
   }
 };
 
-const getData = async () => {
-  const querySnapshot = await getDocs(collection(db, "users"));
+const getData = async ({ dbName }: getDataProps) => {
+  const querySnapshot = await getDocs(collection(db, dbName));
   querySnapshot.forEach((doc) => {
     console.log(`${doc.id} => ${doc.data()}`);
   });
