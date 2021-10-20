a145cf1975424f85cca03860b6eda2f5ead6c8c5
trident-kanda <tridentkandayuki@gmail.com>
2021-10-20 12:10:15 +0900

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
 import Head from "next/head";
-
+import Link from "next/link";
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/firestore">
+            <a className="text-xl">FireStore</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/firestore">
+            <a className="text-xl">FireStore</a>
+          </Link>
+        </li>
+        <li></li>
+      </ul>
+    </div>
+  );
 }
